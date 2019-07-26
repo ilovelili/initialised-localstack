36d26caaab7b25f69da3d7ee46883f64bb7691ea
ilovelili <route666@live.cn>
2019-07-26 10:30:03 +0900

    add `.json` `.txt` `.template` formats support

--- bootstrap/scripts/init.sh
+++ bootstrap/scripts/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in `find "$(cd /opt/bootstrap/templates; pwd)" -name "*.yml" -o -name "*.yaml"`; do
+for i in `find "$(cd /opt/bootstrap/templates; pwd)" -name "*.yml" -o -name "*.yaml" -o -name "*.json" -o -name "*.txt" -o -name "*.template"`; do
     echo "applying cloudformation ... | source => $i"    
     awslocal cloudformation create-stack --template-body file://$i --stack-name $(basename $i)
 done
